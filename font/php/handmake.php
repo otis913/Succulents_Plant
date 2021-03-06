06679356d0637e3377e16e8bf22fff89ea090538
otis913 <otis913913@gmail.com>
2021-03-06 12:15:58 +0800

    後臺更新

--- font/php/handmake.php
+++ font/php/handmake.php
@@ -9,83 +9,149 @@ $dsn = "mysql:host=" . $db_host . ";dbname=" . $db_select;
 //建立PDO物件，並放入指定的相關資料
 $pdo = new PDO($dsn, $db_user, $db_pass);
 
-
+// ---------------------------------------
 $sql = 'select *
      from HANDCLASS';
 
 $statement = $pdo->prepare($sql);
 $statement->execute();
-echo "<tr class='table-dark'>
-            <th>商品編號</th>
-            <th>商品類別</th>
-            <th>商品名稱</th>
-            <th>商品庫存量</th>
-            <th>商品圖片01</th>
-            <th>商品狀態</th>
-            <th>是否編輯</th>
-          </tr>";
-
-//抓出全部且依照順序封裝成一個二維陣列
 $data = $statement->fetchAll();
+// ---------------------------------------
+// foreach ($data as $index => $row) {
 
+echo "<div class='handmake_section ex20px'>
+  <div class='handmake_left ex20px'>
+    <div class='HM_class'>
+      <h1>選擇課程</h1>
+      <div class='HM_class_btn'>
+        <button class='HDclass01'>手捏陶器 + 多肉組盆</button>
+        <button class='HDclass02'>諧意繪泥 & 療癒植栽</button>
+        <button class='HDclass03'>室內多肉照顧課</button>
+      </div>
+    </div>
 
-foreach ($data as $index => $row) {
-  echo "<tr class='table-light'>
-              <td>" . $row["productNO"] . "</td>";
+    <div class='HM_date_number'>
+      <div class='HM_Date '>
+        <h1>選擇日期<i class='fas fa-calendar-alt '></i></h1>
+        <div class='HM_Date_select'>
+          <select name='date' class='custom_select ' id=''>
+            <option value='2021/03/01'>2021/03/01</option>
+            <option value='2021/04/01'>2021/04/01</option>
+            <option value='2021/05/01'>2021/05/01</option>
+          </select>
+        </div>
+      </div>
 
-  $productType = $row["productType"];
-  switch ($productType) {
-    case "1":
-      $productType = '景天科';
-      break;
-    case "2":
-      $productType = '仙人掌科';
-      break;
-    case "3":
-      $productType = '百合科';
-      break;
-    case "4":
-      $productType = '菊科';
-      break;
-    case "5":
-      $productType = '飾品';
-      break;
-    case "6":
-      $productType = '器皿';
-      break;
-    case "7":
-      $productType = '客製植物';
-      break;
-    case "8":
-      $productType = '賀卡';
-      break;
-    case "9":
-      $productType = '手做課程';
-      break;
-    default:
-      $productType = '錯誤';
-  };
-  echo "<td>" . $productType . "</td>";
+      <div class='HM_Number'>
+        <h1>選擇人數</h1>
+        <div class='HM_choseNumber'>
+          <i class='fas fa-minus'></i>
+          <input type='number' value='1' min='1' class='number_people'>
+          <i class='fas fa-plus'></i>
+        </div>
+      </div>
+    </div>
 
-  echo "<td>" . $row["productName"] . "</td>";
-  echo "<td>" . $row["productNumber"] . "</td>";
-  echo '<td ><img src="./img/' . $row['productImg01'] . '"></td>';
+    <div class='handmake_left_text ex20px'>
+      <h1>尚於<span>10</span>人可報名</h1>
+      <h1>課程費用<span>$2800</span>/人</h1>
+    </div>
 
-  $productStatus = $row["productStatus"];
-  switch ($productStatus) {
-    case "1":
-      $productStatus = '上架';
-      break;
-    case "2":
-      $productStatus = '下架';
-      break;
-    default:
-      $productStatus = '錯誤';
-  };
-  echo "<td>" . $productStatus . "</td>";
+    <!-- 黃色bubble按鈕 start -->
+    <div class='bubble_btnn SignUp'>
+      <svg xmlns='http://www.w3.org/2000/svg' version='1.1' class='goo'>
+        <defs>
+          <filter id='goo'>
+            <feGaussianBlur in='SourceGraphic' stdDeviation='10' result='blur' />
+            <feColorMatrix in='blur' mode='matrix' values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
+              result='goo' />
+            <feComposite in='SourceGraphic' in2='goo' />
+          </filter>
+        </defs>
+      </svg>
 
-  echo "<td>
-          <a href='./product_edit.php?PID=" . $row['productNO'] . "'>編輯</a>
-        </td>
-      </tr>";
-}
+      <span class='button--bubble__container'>
+        <a href='' class='button_y button--bubble'>
+          確認報名
+        </a>
+        <span class='button--bubble__effect-container'>
+          <span class='circle top-left'></span>
+      <span class='circle top-left'></span>
+      <span class='circle top-left'></span>
+
+      <span class='button effect-button'></span>
+
+      <span class='circle bottom-right'></span>
+      <span class='circle bottom-right'></span>
+      <span class='circle bottom-right'></span>
+      </span>
+      </span>
+    </div>
+    <!-- 黃色bubble按鈕 end -->
+  </div>
+
+  <div class='handmake_right '>
+    <img src='./img/handclass/HandMake_title.png' alt=''>
+    <div class='handmake_right_main'>
+      <h1>開課時間/地點</h1>
+      <p>2021/2/8,2/12,2/24</p>
+      <p>台北市大安區金山南路二段123巷50號</p>
+
+      <h1>課程介紹</h1>
+      <ul>
+        <li>1.從盆器設計,釉色挑選,多肉植物,每一個步驟都可以參與製作</li>
+        <li>2.教你怎麼玩多肉!當季的當然最健康最好養</li>
+        <li>3.從介質挑選及介紹,多肉植物脫盆到組盆,每一個細節都是自己親手參與</li>
+      </ul>
+      <div class='notice'>
+        <h1>注意事項</h1>
+        <div class='notice_main'>
+          <ul>
+            <li>1.開課日前3天,不予退費及更改梯次,如無法參加課程,請將名額轉讓給其他人參加</li>
+            <li>2.如課程招生未滿或因天災不可抗力因素,我們將於2天前主動聯繫協助更改梯或全額退費</li>
+            <li>3.若遲到30分鐘則無法參與課程,視同取消</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class='handmake_left_text handmark_text_RWD ex20px'>
+      <h1>尚於<span>10</span>人可報名</h1>
+      <h1>課程費用<span>$2800</span>/人</h1>
+    </div>
+
+    <!-- 黃色bubble按鈕 start -->
+    <div class='bubble_btnn SignUp'>
+      <svg xmlns='http://www.w3.org/2000/svg' version='1.1' class='goo'>
+        <defs>
+          <filter id='goo'>
+            <feGaussianBlur in='SourceGraphic' stdDeviation='10' result='blur' />
+            <feColorMatrix in='blur' mode='matrix' values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9'
+              result='goo' />
+            <feComposite in='SourceGraphic' in2='goo' />
+          </filter>
+        </defs>
+      </svg>
+
+      <span class='button--bubble__container'>
+        <a href='' class='button_y button--bubble'>
+          確認報名
+        </a>
+        <span class='button--bubble__effect-container'>
+          <span class='circle top-left'></span>
+      <span class='circle top-left'></span>
+      <span class='circle top-left'></span>
+
+      <span class='button effect-button'></span>
+
+      <span class='circle bottom-right'></span>
+      <span class='circle bottom-right'></span>
+      <span class='circle bottom-right'></span>
+      </span>
+      </span>
+    </div>
+    <!-- 黃色bubble按鈕 end -->
+  </div>
+</div>";
+
+// }
